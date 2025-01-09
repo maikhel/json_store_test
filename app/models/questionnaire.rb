761841615c4d18403f53afebd8652f4e9b7e59cb
maikhel <mlecicki@gmail.com>
2025-01-09 10:45:16 +0100

    Use questions column as store in questionnaire model

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,2 +1,5 @@
 class Questionnaire < ApplicationRecord
+  QUESTION_LIST = %w[question1 question2]
+
+  store :questions, accessors: QUESTION_LIST
 end
--- test/models/questionnaire_test.rb
+++ test/models/questionnaire_test.rb
@@ -1,4 +1,15 @@
 require "test_helper"
 
 class QuestionnaireTest < ActiveSupport::TestCase
+  test "stores questions in jsonb column" do
+    questionnaire = Questionnaire.new
+    questionnaire.question1 = "How are you?"
+    questionnaire.question2 = "How old are you?"
+
+    questionnaire.save!
+
+    questionnaire.reload
+    assert questionnaire.question1, "How are you?"
+    assert questionnaire.question2, "How old are you?"
+  end
 end
